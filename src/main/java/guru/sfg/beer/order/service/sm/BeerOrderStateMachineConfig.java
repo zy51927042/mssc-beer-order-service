9065a5108541ca700f12f031a024d3e1bafeb92b
zy51927042 <zy51927042@163.com>
2022-07-10 23:17:10 +0800

    Place Validate Order Request Closes #9

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -2,17 +2,21 @@ package guru.sfg.beer.order.service.sm;
 
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
 import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
-import org.springframework.stereotype.Component;
 
 import java.util.EnumSet;
 
-@Component
+@Configuration
 @EnableStateMachineFactory
+@RequiredArgsConstructor
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
+    private final Action<BeerOrderStatusEnum, BeerOrderEventEnum> validateOrderAction;
     @Override
     public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> states) throws Exception {
        states.withStates()
@@ -30,7 +34,7 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
         transitions.withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
-                //todo add validation action here
+                .action(validateOrderAction)
             .and().withExternal()
                 .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
                 .event(BeerOrderEventEnum.VALIDATION_PASSED)
---
+++ src/main/java/guru/sfg/beer/order/service/sm/actions/ValidateOrderAction.java
@@ -0,0 +1,39 @@
+package guru.sfg.beer.order.service.sm.actions;
+
+import guru.sfg.beer.order.service.config.JmsConfig;
+import guru.sfg.beer.order.service.domain.BeerOrder;
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import guru.sfg.beer.order.service.services.BeerOrderManagerImpl;
+import guru.sfg.beer.order.service.web.mappers.BeerOrderMapper;
+import guru.sfg.brewery.model.BeerOrderDto;
+import guru.sfg.brewery.model.events.ValidateOrderRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ValidateOrderAction implements Action<BeerOrderStatusEnum, BeerOrderEventEnum>  {
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+
+    private final JmsTemplate jmsTemplate;
+    @Override
+    public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext) {
+        String orderId = (String) stateContext.getMessageHeader(BeerOrderManagerImpl.ORDER_ID_HEADER);
+        BeerOrder beerOrder= beerOrderRepository.findOneById(UUID.fromString(orderId));
+        BeerOrderDto beerOrderDto = beerOrderMapper.beerOrderToDto(beerOrder);
+
+        jmsTemplate.convertAndSend(JmsConfig.VALIDATE_ORDER_QUEUE,
+                ValidateOrderRequest.builder().beerOrderDto(beerOrderDto).build());
+        log.debug("Send Validation request to queue for order id" + orderId);
+    }
+}
